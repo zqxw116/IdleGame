d2fa98ff9614637d043c11e9a91087c226c91b92
Uniman <zqxw116@naver.com>
2025-07-06 19:11:26 +0900

    65~66강 Web Server 생성 및 적용

---
+++ Server/WebServer/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebServer.Controllers
+{
+    // RESTful 서버 통신규약
+    // POST: TestController/test
+
+    // ip:port/test
+    [ApiController]
+    [Route("test")]
+    public class TestController : ControllerBase
+    {
+        // ip:port/test/hello
+        [HttpPost]
+        [Route("hello")]
+        public TestPackRes TestPost([FromBody] TestPackReq value)
+        {
+            TestPackRes result = new TestPackRes();
+            result.success = true;
+            return result;
+
+        }
+    }
+}
---
+++ Server/WebServer/Data/WebPacket.cs
@@ -0,0 +1,18 @@
+﻿
+// Require
+// 클라 -> 서버
+[Serializable] // 유니티에서는 붙여야 된다
+public class TestPackReq
+{
+    public string userId { get; set; }
+    public string token { get; set; }
+
+}
+
+// Respone
+// 서버 -> 클라
+[Serializable]
+public class TestPackRes
+{
+    public bool success { get; set; }
+}
